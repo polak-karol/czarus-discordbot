8d5bcfea74e8e811d0bf98014c568c1b8941cb7f
Karol Polak <karolpolak987@gmail.com>
2022-07-19 19:08:51 +0200

    WIP

--- database/getClient.js
+++ database/getClient.js
@@ -8,7 +8,6 @@ module.exports.getClient = async () => {
     user: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
     database: process.env.PG_DATABASE,
-    ssl: true,
   });
   await client.connect();
   return client;
