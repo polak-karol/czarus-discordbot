70af16b741b425d5f52074c65341abae8771d62c
karol-polak-elmark <110454689+karol-polak-elmark@users.noreply.github.com>
2022-09-18 16:21:39 +0200

    Add holiday command

--- commands/jobs/holiday.js
+++ commands/jobs/holiday.js
@@ -1,18 +1,22 @@
 const { isAdmin } = require("../../utils");
+const cron = require("node-cron");
+
+const main = (message, args) => {
+  if (!isAdmin(message))
+    return message.channel.send("Tylko administrator może używać tej komendy.");
+
+  cron.schedule(
+    "0 0 6 * * *",
+    () => {
+      message.channel.send("Test");
+    },
+    { timezone: "Europe/Warsaw" }
+  );
+};
 
 module.exports = {
-  name: "bump",
-  description: "test opis",
-  usage: "!bump",
-  execute: async (message, args) => {
-    if (!isAdmin(message))
-      return message.channel.send(
-        "Tylko administrator może używać tej komendy."
-      );
-
-    message.channel.send("/bump");
-    setInterval(() => {
-      message.channel.send("/bump");
-    }, 10000);
-  },
+  name: "swietuj",
+  description: "Ustaw powiadomienia o świętach",
+  usage: "!swietuj",
+  execute: async (message, args) => main(message, args),
 };
