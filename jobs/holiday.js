76b2f622b5c220745e221010fb342b89d1efbd4c
karol-polak-elmark <110454689+karol-polak-elmark@users.noreply.github.com>
2022-09-30 22:07:38 +0200

    Holiday test

--- jobs/holiday.js
+++ jobs/holiday.js
@@ -2,11 +2,11 @@ const cron = require("node-cron");
 
 const sendDailyHolidayInfo = (client) => {
   cron.schedule(
-    "0 45 21 * * *",
+    "0 0 6 * * *",
     () => {
       client.guilds.cache
-        .get("733001624427036825")
-        .channels.cache.get("993197778685657180")
+        .get("972581289972596756")
+        .channels.cache.get("1001790827091198033")
         .send(
           "Dziś obchodzimy ***Światowy Dzień Wegetarianizmu.*** \n\nCzy mamy na sali wegetarian? Jeśli tak, zachęcam do podzielenia się opinią, co skłoniło was do niejedzenia mięsa. \n\nCzy warto być na tej “diecie”? Jakie korzyści przynosi wegetarianizm i czy ma wpływ na wasze samopoczucie?"
         );
