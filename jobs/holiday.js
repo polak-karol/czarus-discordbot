d094953aa0995f341e5a1242e00b0a0a6c441fee
karol-polak-elmark <110454689+karol-polak-elmark@users.noreply.github.com>
2022-10-01 09:21:10 +0200

    TEST

--- jobs/holiday.js
+++ jobs/holiday.js
@@ -2,15 +2,15 @@ const cron = require("node-cron");
 const moment = require("moment");
 const { getHoliday } = require("../utils/commands/holidayUtils");
 
-const sendDailyHolidayInfo = async (client) => {
-  console.log(await getHoliday("972581289972596756"));
+const sendDailyHolidayInfo = (client) => {
+  // console.log(await getHoliday("972581289972596756"));
   cron.schedule(
-    "0 18 9 * * *",
-    () => {
+    "0 22 9 * * *",
+    async () => {
       client.guilds.cache
         .get("733001624427036825")
         .channels.cache.get("993197778685657180")
-        .send(getHoliday("972581289972596756")[0].message);
+        .send(await getHoliday("972581289972596756")[0].message);
     },
     { timezone: "Europe/Warsaw" }
   );
