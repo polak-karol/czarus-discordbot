306f3b97268f829baa734f57b89d8db8f47a3d5f
karol-polak-elmark <110454689+karol-polak-elmark@users.noreply.github.com>
2023-02-08 20:31:13 +0100

    Add missionpassed command

--- index.js
+++ index.js
@@ -22,6 +22,15 @@ const client = new Client({
 const slashCommands = [
   new SlashCommandBuilder().setName("ping").setDescription("Ping pong!"),
   new SlashCommandBuilder()
+    .setName("missionpassed")
+    .setDescription("Missionpassed.")
+    .addUserOption((option) =>
+      option
+        .setName("uzytkownik")
+        .setDescription("Dowolny użytkownik serwera.")
+        .setRequired(true)
+    ),
+  new SlashCommandBuilder()
     .setName("wiezienie")
     .setDescription("Wiezienie.")
     .addUserOption((option) =>
