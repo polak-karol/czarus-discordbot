df7d5a08fd5c12a146e00a45b598c48d903a95d9
karol-polak-elmark <110454689+karol-polak-elmark@users.noreply.github.com>
2023-02-08 20:38:30 +0100

    Add sniper command

--- index.js
+++ index.js
@@ -22,6 +22,15 @@ const client = new Client({
 const slashCommands = [
   new SlashCommandBuilder().setName("ping").setDescription("Ping pong!"),
   new SlashCommandBuilder()
+    .setName("snajper")
+    .setDescription("Snajper.")
+    .addUserOption((option) =>
+      option
+        .setName("uzytkownik")
+        .setDescription("Dowolny użytkownik serwera.")
+        .setRequired(true)
+    ),
+  new SlashCommandBuilder()
     .setName("straszny")
     .setDescription("Straszny.")
     .addUserOption((option) =>
---
+++ slash-commands/fun/sniper.js
@@ -0,0 +1,30 @@
+const fetch = require("node-fetch");
+const { errorCommandMessage } = require("../../utils/config");
+
+module.exports = {
+  name: "snajper",
+  execute: async (interaction) => {
+    await interaction.deferReply();
+    const body = {
+      url: interaction.options.getUser("uzytkownik").avatarURL(),
+    };
+
+    try {
+      const response = await fetch(`${process.env.AMETHYSTE_URL}/sniper`, {
+        method: "POST",
+        headers: {
+          authorization: `Bearer ${process.env.AMETHYSTE_TOKEN}`,
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+      const imageBlob = await response.arrayBuffer();
+      return await interaction.editReply({
+        files: [{ attachment: Buffer.from(imageBlob) }],
+      });
+    } catch (error) {
+      console.log(error);
+      return await interaction.editReply(errorCommandMessage);
+    }
+  },
+};
