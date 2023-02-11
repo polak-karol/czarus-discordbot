f73ab1034cdee36bb4f5db962daf65fb8893e413
karol-polak-elmark <110454689+karol-polak-elmark@users.noreply.github.com>
2023-02-11 01:28:26 +0100

    Commit

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ const client = new Client({
     GatewayIntentBits.GuildMessageTyping,
   ],
 });
+console.log(process.env.ENVIRONMENT);
+console.log(process.env.ENVIRONMENT);
+console.log(process.env.ENVIRONMENT);
+console.log("FEFEFEFEFEFEEFEFFE");
+console.log(process.env.ENVIRONMENT);
 
 const slashCommands = [
   new SlashCommandBuilder().setName("ping").setDescription("Ping pong!"),
