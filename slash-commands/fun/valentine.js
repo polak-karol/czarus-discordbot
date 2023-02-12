b17df74c428978c36bb8c62b2430e5034cc4945f
karol-polak-elmark <110454689+karol-polak-elmark@users.noreply.github.com>
2023-02-12 20:34:54 +0100

    Add sending valentines to valentine recipient

--- slash-commands/fun/valentine.js
+++ slash-commands/fun/valentine.js
@@ -1,3 +1,4 @@
+const { ChannelType } = require("discord.js");
 const { getClient } = require("../../database/getClient");
 
 const responses = {
@@ -32,11 +33,40 @@ const saveValentine = async (interaction) => {
   return 1;
 };
 
+const sendValentine = async (interaction, client) => {
+  const channel = await client.guilds.cache
+    .get("733001624427036825")
+    .channels.cache.get("733001624930484305");
+
+  const valentineRecipient = interaction.options.getUser("do");
+
+  let privateThread = channel.threads.cache.find(
+    (x) => x.name === valentineRecipient.id
+  );
+
+  if (!privateThread) {
+    privateThread = await channel.threads.create({
+      name: valentineRecipient.id,
+      autoArchiveDuration: 60,
+      type: ChannelType.PrivateThread,
+      reason: "Walentynka",
+    });
+    await privateThread.members.add(valentineRecipient.id);
+    await privateThread.send("POCZTA WALENTYNKOWA");
+  }
+
+  await privateThread.send(interaction.options.getString("wiadomosc"));
+};
+
 module.exports = {
   name: "walentynka",
-  execute: async (interaction) => {
+  execute: async (interaction, client) => {
     await interaction.deferReply({ ephemeral: true });
+
     const saveResult = await saveValentine(interaction);
+
+    if (saveResult === 1) await sendValentine(interaction, client);
+
     return await interaction.editReply({
       content: responses[saveResult],
       ephemeral: true,
