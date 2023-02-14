0ab5326ba288f1f70bfd1131f0573e2d67a7c745
karol-polak-elmark <110454689+karol-polak-elmark@users.noreply.github.com>
2023-02-14 19:28:50 +0100

    Add push

--- jobs/valentine.js
+++ jobs/valentine.js
@@ -11,61 +11,74 @@ function sleep(ms) {
   });
 }
 
-const getEmbed = (guildMember) =>
+const getEmbed = () =>
   new EmbedBuilder()
     .setDescription(
       "Dwie róże w wazonie są stołu ozdobą, \nA list jest rozmową między Magią Słów a Tobą. \nWięc dla dalszej miłej naszej znajomości, \nWysyłamy Ci w Walentynki ten symbol miłości. \nTen liścik z całusem dziś do Ciebie leci, \nHumor Ci poprawi, a sercu żar roznieci. \nPomyśl dzisiaj o Magii Słów tak słodko i miło, \nŻeby w środku zimy cieplej się zrobiło!"
     )
     .setColor("#EA2027")
     .setTitle("Poczta Walentynkowa")
-    .setAuthor({
-      name: guildMember.user.username,
-      iconURL: guildMember.user.avatarURL({ dynamic: true }),
-    })
     .setFooter({ text: "Administracja Magii Słów" });
 
-const sendValentineToGuildMember = async (guildMember, channel) => {
-  let privateThread = channel.threads.cache.find(
-    (x) => x.name === guildMember.user.id
-  );
+const sendValentineToGuildMember = async (userId, channel) => {
+  let privateThread = channel.threads.cache.find((x) => x.name === userId);
 
   if (!privateThread) {
     privateThread = await channel.threads.create({
-      name: guildMember.user.id,
+      name: userId,
       autoArchiveDuration: ThreadAutoArchiveDuration.ThreeDays,
       type: ChannelType.PrivateThread,
       reason: "Walentynka",
     });
-    await privateThread.members.add(guildMember.user.id);
+    await privateThread.members.add(userId);
   }
 
-  await privateThread.send(`||<@${guildMember.user.id}>||`);
-  await privateThread.send({ embeds: [getEmbed(guildMember)] });
+  await privateThread.send(`||<@${userId}>||`);
+  await privateThread.send({ embeds: [getEmbed()] });
 
   return true;
 };
 
-const saveAllToDatabase = async (guildMember) => {
+const saveAllToDatabase = async (userId) => {
   const client = await getClient();
   const countResult = await client.query(
-    `SELECT COUNT(guild_id) FROM valentinesall WHERE guild_id = '972581289972596756' AND recipient_id = '${guildMember.user.id}';`
+    `SELECT COUNT(guild_id) FROM valentinesall WHERE guild_id = '972581289972596756' AND recipient_id = '${userId}';`
   );
   if (parseInt(countResult.rows[0].count, 10) > 0) return false;
   await client.query(
-    `INSERT INTO valentinesall(recipient_id, guild_id) VALUES ('${guildMember.user.id}', '972581289972596756');`
+    `INSERT INTO valentinesall(recipient_id, guild_id) VALUES ('${userId}', '972581289972596756');`
   );
   return true;
 };
 
+const getAll = async () => {
+  const client = await getClient();
+  const countResult = await client.query(
+    `SELECT recipient_id FROM valentinesall WHERE guild_id = '972581289972596756';`
+  );
+
+  return countResult;
+};
+
 const sendValentineToEveryone = async (client) => {
-  return;
   const guild = await client.guilds.cache.get("972581289972596756");
   const guildMembers = await guild.members.fetch();
   const channel = await guild.channels.cache.get("1074612430979731496");
+  const values = await getAll();
+  const userWithValentinesIds = values.rows.map(
+    ({ recipient_id }) => recipient_id
+  );
+  const userIds = Array.from(guildMembers).map(([id]) => id);
+  const userWithNeeds = [];
 
-  guildMembers.forEach(async (guildMember) => {
-    const result = await saveAllToDatabase(guildMember);
-    if (result) await sendValentineToGuildMember(guildMember, channel);
+  userIds.forEach((userId) => {
+    if (userWithValentinesIds.includes(userId)) return;
+    return userWithNeeds.push(userId);
+  });
+
+  userWithNeeds.forEach(async (userId) => {
+    const result = await saveAllToDatabase(userId);
+    if (result) await sendValentineToGuildMember(userId, channel);
   });
 };
 
