8b1085c547124fa49bd65ee9780f82b88fe9b2ea
karol-polak-elmark <110454689+karol-polak-elmark@users.noreply.github.com>
2023-02-14 18:55:27 +0100

    Fix sendValentineToEveryone

--- jobs/valentine.js
+++ jobs/valentine.js
@@ -58,6 +58,7 @@ const saveAllToDatabase = async (guildMember) => {
 };
 
 const sendValentineToEveryone = async (client) => {
+  return;
   const guild = await client.guilds.cache.get("972581289972596756");
   const guildMembers = await guild.members.fetch();
   const channel = await guild.channels.cache.get("1074612430979731496");
