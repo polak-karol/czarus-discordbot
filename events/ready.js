22f0a66546aae07e10d42c009ec13d31d34fdf38
karol-polak-elmark <110454689+karol-polak-elmark@users.noreply.github.com>
2023-02-14 18:26:28 +0100

    Test v2

--- events/ready.js
+++ events/ready.js
@@ -13,10 +13,8 @@ module.exports = {
     sendValentineToEveryone(client);
     client.user.setActivity("starożytne księgi", { type: "WATCHING" });
     console.log("Bot is online");
-    const guild = await client.guilds.cache.get("733001624427036825");
+    const guild = await client.guilds.cache.get("972581289972596756");
     const guildMembers = await guild.members.fetch();
-    console.log(guildMembers);
-    console.log(typeof guildMembers);
     console.log(
       Array.from(guildMembers).forEach(() => {
         console.log(Array.from(guildMembers).length);
