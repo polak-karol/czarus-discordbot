2ccabecca5c19d7739c37d6229cd75b0cda68a91
karol-polak-elmark <110454689+karol-polak-elmark@users.noreply.github.com>
2023-02-14 18:23:54 +0100

    Test

--- events/ready.js
+++ events/ready.js
@@ -6,12 +6,21 @@ const { sendValentineToEveryone } = require("../jobs/valentine");
 module.exports = {
   name: "ready",
   once: true,
-  execute(client) {
+  async execute(client) {
     setupDatabase();
     sendDailyHolidayInfo(client);
     sendDailyBirthDayInfo(client);
     sendValentineToEveryone(client);
     client.user.setActivity("starożytne księgi", { type: "WATCHING" });
     console.log("Bot is online");
+    const guild = await client.guilds.cache.get("733001624427036825");
+    const guildMembers = await guild.members.fetch();
+    console.log(guildMembers);
+    console.log(typeof guildMembers);
+    console.log(
+      Array.from(guildMembers).forEach(() => {
+        console.log(Array.from(guildMembers).length);
+      })
+    );
   },
 };
