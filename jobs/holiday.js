a32e362a631f113088c0ce9245d51b097e5debbb
Karol Polak <72245667+polak-karol@users.noreply.github.com>
2023-08-04 17:49:30 +0200

    Update holiday.js

--- jobs/holiday.js
+++ jobs/holiday.js
@@ -3,7 +3,7 @@ const { getHoliday } = require("../utils/commands/holidayUtils");
 
 const sendDailyHolidayInfo = async (client) => {
   cron.schedule(
-    "0 10 12 * * *",
+    "0 0 18 * * *",
     async () => {
       const [holiday] = await getHoliday("972581289972596756");
 
