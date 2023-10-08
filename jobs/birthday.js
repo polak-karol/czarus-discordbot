b69c695024e36ec8f4ee73c0ae5b07e7624bcd00
polak-karol <karolpolak987@gmail.com>
2023-10-08 12:07:37 +0200

    Fix jobs

--- jobs/birthday.js
+++ jobs/birthday.js
@@ -1,4 +1,5 @@
 const cron = require("node-cron");
+const _ = require("lodash");
 const { getRandomInteger, getBirthdaysConfig } = require("../utils");
 const {
   getBirthday,
@@ -9,34 +10,35 @@ const {
 const sendDailyBirthDayInfo = async (client) => {
   const birthdaysConfig = await getBirthdaysConfig();
 
-  birthdaysConfig.forEach((birthdaysConfigItem) =>
-    cron.schedule(
-      "0 0 8 * * *",
-      async () => {
-        const birthdays = await getBirthday(birthdaysConfigItem.guildId);
+  if (!_.isEmpty(birthdaysConfig))
+    birthdaysConfig.forEach((birthdaysConfigItem) =>
+      cron.schedule(
+        "0 0 8 * * *",
+        async () => {
+          const birthdays = await getBirthday(birthdaysConfigItem.guildId);
 
-        if (birthdays.length > 0) {
-          const text = `<@&986163091089809428> Urodziny dzisiaj ${
-            birthdays.length > 1 ? "obchodzą" : "obchodzi"
-          }:\n${birthdays
-            .map((birthday) => `<@${birthday.userId}>`)
-            .join(", ")}. \n\n${
-            birthdays.length > 1
-              ? wishesPlural[getRandomInteger(0, wishesPlural.length)]
-              : wishesSingular[getRandomInteger(0, wishesSingular.length)]
-          }`;
+          if (birthdays.length > 0) {
+            const text = `<@&986163091089809428> Urodziny dzisiaj ${
+              birthdays.length > 1 ? "obchodzą" : "obchodzi"
+            }:\n${birthdays
+              .map((birthday) => `<@${birthday.userId}>`)
+              .join(", ")}. \n\n${
+              birthdays.length > 1
+                ? wishesPlural[getRandomInteger(0, wishesPlural.length)]
+                : wishesSingular[getRandomInteger(0, wishesSingular.length)]
+            }`;
 
-          client.guilds.cache
-            .get(birthdaysConfigItem.guildId)
-            .channels.cache.get(
-              birthdaysConfigItem.birthdaysAnnouncementChannelId
-            )
-            .send(text);
-        }
-      },
-      { timezone: "Europe/Warsaw" }
-    )
-  );
+            client.guilds.cache
+              .get(birthdaysConfigItem.guildId)
+              .channels.cache.get(
+                birthdaysConfigItem.birthdaysAnnouncementChannelId
+              )
+              .send(text);
+          }
+        },
+        { timezone: "Europe/Warsaw" }
+      )
+    );
 };
 
 module.exports = { sendDailyBirthDayInfo };
